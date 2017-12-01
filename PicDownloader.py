c5a634699de9e11cf735ff036cfff8ecfa2b2c27
dingchenwei <dingchenwei@sensetime.com>
2017-12-01 20:14:17 +0800

    multiprocessing triple and double

--- PicDownloader.py
+++ PicDownloader.py
@@ -11,17 +11,12 @@ def getPage(url, keyword, page_index):
     # for i in range(30,30*pages+30,30):
     keyword = keyword.replace('_', ' ')
     params = {
-          'q':keyword,
-          'src':'srp',
-          'correct':keyword,
-          'sn':page_index,
-          'pn':60,
-          'sid':'bdb4ac732ab9fab378895ff9976789a6',
-          'ran':0,
-          'ras':6,
-          'cn':0,
-          'gn':0,
-          'kn':0
+        'query': keyword,
+        'mode': 1,
+        'start': page_index,
+        'reqType': 'ajax',
+        'reqFrom': 'result',
+        'tn': 0
               }
     result = requests.get(url, params=params)
         # print requests.get(url, params=i).json().get('data')
@@ -43,7 +38,7 @@ def downloadPic(url,keyword, pages):
             print keyword+"下载失败，请重新下载"
             break
         pic_url = re.findall('"pic_url":"(.*?)",',result.text,re.S)
-        print result.text
+        # print result.text
         print len(pic_url)
         if len(pic_url) == 0:
             print "#####done#####"
@@ -78,57 +73,63 @@ def downloadPic(url,keyword, pages):
 
 
 if __name__ == '__main__':
-    url = 'https://sg.images.search.yahoo.com/search/'
+    url = 'http://pic.sogou.com/pics?'
 
 
-    keyword_list = ['女主播', '女孩', '女生', '女老师', '女运动员', '女清洁工', '女教授', '女服务员', '女医生', '女演员', '女明星']
-    # keyword_list = ['1', '2', '3', '4', '5', '6', '7']
-    pool = multiprocessing.Pool(processes=50)
+    # keyword_list = ['女主播', '女孩', '女生', '女老师', '女运动员', '女清洁工', '女教授', '女服务员', '女医生', '女演员', '女明星']
+    # # keyword_list = ['1', '2', '3', '4', '5', '6', '7']
+    # pool = multiprocessing.Pool(processes=50)
+    # for word in keyword_list:
+    #     ori_word = word
+    #     #downloadPic(url,ori_word, 30)
+    #     word_zipai = '自拍_'+ ori_word
+    #     pool.apply_async(downloadPic, (url, word_zipai, 1000,))
+    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_zipai, 1000,))
+    #     # p.start()
+    #     # p.join()
+    #     # downloadPic(url, ori_word, 1000)
+    #     word_changfa = '自拍_'+ori_word + '_长发'
+    #     pool.apply_async(downloadPic, (url, word_changfa, 1000,))
+    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_changfa, 1000,))
+    #     # p.start()
+    #     # p.join()
+    #     # downloadPic(url, word_changfa, 1000)
+    #     word_duanfa = '自拍_'+ori_word + '_短发'
+    #     pool.apply_async(downloadPic, (url, word_duanfa, 1000,))
+    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_duanfa, 1000,))
+    #     # p.start()
+    #     # p.join()
+    #     # downloadPic(url, word_duanfa, 1000)
+    #     word_zhongfa = '自拍_' + ori_word + '_中发'
+    #     pool.apply_async(downloadPic, (url, word_zhongfa, 1000,))
+    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_zhongfa, 1000,))
+    #     # p.start()
+    #     # p.join()
+    #     # downloadPic(url, word_duanfa, 1000)
+    # pool.close()
+    # pool.join()
+    # #
+    # #
+    keyword_list = ['情侣', '父子', '母子', '爸妈', '爷爷奶奶', '姐弟', '双胞胎', '龙凤胎']
+    pool = multiprocessing.Pool(processes=10)
     for word in keyword_list:
         ori_word = word
         #downloadPic(url,ori_word, 30)
         word_zipai = '自拍_'+ ori_word
         pool.apply_async(downloadPic, (url, word_zipai, 1000,))
-        # p = multiprocessing.Process(target=downloadPic, args=(url, word_zipai, 1000,))
-        # p.start()
-        # p.join()
-        # downloadPic(url, ori_word, 1000)
-        word_changfa = '自拍_'+ori_word + '_长发'
-        pool.apply_async(downloadPic, (url, word_changfa, 1000,))
-        # p = multiprocessing.Process(target=downloadPic, args=(url, word_changfa, 1000,))
-        # p.start()
-        # p.join()
-        # downloadPic(url, word_changfa, 1000)
-        word_duanfa = '自拍_'+ori_word + '_短发'
-        pool.apply_async(downloadPic, (url, word_duanfa, 1000,))
-        # p = multiprocessing.Process(target=downloadPic, args=(url, word_duanfa, 1000,))
-        # p.start()
-        # p.join()
+        # downloadPic(url, word_zipai, 1000)
+        # word_changfa = ori_word + '长发'
+        # downloadPic(url, word_changfa, 30)
+        # word_duanfa = '自拍_'+ori_word + '短发'
         # downloadPic(url, word_duanfa, 1000)
-        word_zhongfa = '自拍_' + ori_word + '_中发'
-        pool.apply_async(downloadPic, (url, word_zhongfa, 1000,))
-        # p = multiprocessing.Process(target=downloadPic, args=(url, word_zhongfa, 1000,))
-        # p.start()
-        # p.join()
+        # word_duanfa = '自拍_' + ori_word + '中发'
         # downloadPic(url, word_duanfa, 1000)
+    keyword_list = ['三口之家', '一家三口', '三胞胎', '三个人']
+    for word in keyword_list:
+        # keyword_list2 = ['_1', '_2', '_3', '_4']
+        # for word2 in keyword_list2:
+        # downloadPic(url, '自拍_'+word, 1000)
+        pool.apply_async(downloadPic, (url, '自拍_'+word, 1000,))
+
     pool.close()
     pool.join()
-    # #
-    # #
-    # keyword_list = ['情侣', '父子', '母子', '爸妈', '爷爷奶奶', '姐弟', '双胞胎', '龙凤胎']
-    # for word in keyword_list:
-    #     ori_word = word
-    #     #downloadPic(url,ori_word, 30)
-    #     word_zipai = '自拍_'+ ori_word
-    #     downloadPic(url, word_zipai, 1000)
-    #     # word_changfa = ori_word + '长发'
-    #     # downloadPic(url, word_changfa, 30)
-    #     # word_duanfa = '自拍_'+ori_word + '短发'
-    #     # downloadPic(url, word_duanfa, 1000)
-    #     # word_duanfa = '自拍_' + ori_word + '中发'
-    #     # downloadPic(url, word_duanfa, 1000)
-    # keyword_list = ['三口之家', '一家三口', '三胞胎', '三个人']
-    # for word in keyword_list:
-    #     keyword_list2 = ['_1', '_2', '_3', '_4']
-    #     for word2 in keyword_list2:
-    #         downloadPic(url, '自拍_'+word+word2, 1000)
