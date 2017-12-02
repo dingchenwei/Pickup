06c1fee88b8a86925743fa64f974c48558f10d60
leo <dingchenwei@sensetime.com>
2017-12-02 16:53:10 +0800

    final

--- PicDownloader.py
+++ PicDownloader.py
@@ -7,43 +7,46 @@ import numpy as np
 import os
 from requests.exceptions import *
 import multiprocessing
-def getPage(url, keyword, page_index):
+import json
+def getPage(url, keyword, page_index, person_num, sex):
     # for i in range(30,30*pages+30,30):
     keyword = keyword.replace('_', ' ')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    }
     params = {
-        'query': keyword,
-        'mode': 1,
-        'start': page_index,
-        'reqType': 'ajax',
-        'reqFrom': 'result',
-        'tn': 0
+        'limit':50,
+        'line':page_index
               }
-    result = requests.get(url, params=params)
+    payload = json.dumps({"keyword":keyword,"color":"0","category_id":"","line":"2550","type":"6","image_url":"","dp_search_sort":"","dp_search_orientation":"","dp_search_editorial":"","dp_search_race":"","dp_search_quantity":person_num,"dp_search_gender":sex,"dp_search_age":"","dp_search_color":""})
+    result = requests.post(url, params=params, data=payload, headers=headers)
         # print requests.get(url, params=i).json().get('data')
     return result
 
-def downloadPic(url,keyword, pages):
-    if(not os.path.exists('pictures/sougou/'+keyword)):
-        os.system('mkdir pictures/sougou/'+keyword)
-    f = open('pictures/sougou/'+keyword+'/urls.txt', 'w')
+def downloadPic(url,keyword, pages, person_num, sex=""):
+    if(not os.path.exists('pictures/paixin/'+keyword+'_'+str(person_num)+sex+str(pages))):
+        os.system('mkdir pictures/paixin/'+keyword+'_'+str(person_num)+sex+str(pages))
+    f = open('pictures/paixin/'+keyword+'_'+str(person_num)+sex+str(pages)+'/urls.txt', 'w')
     i = 0
     print '找到关键词:'+keyword+'的图片，现在开始下载图片...'
-    for page_index in range(48, 48*pages+48, 48):
+    for page_index in range(pages-10000+50, pages+50, 50):
         try:
-            result = getPage(url, keyword, page_index)
+            result = getPage(url, keyword, page_index, person_num, sex)
         except (ConnectionError, ChunkedEncodingError, BaseHTTPError,
                 ContentDecodingError, HTTPError, Timeout, SSLError,
                 MissingSchema, TooManyRedirects, RequestException,
                 InvalidSchema, InvalidURL, URLRequired, ProxyError):
             print keyword+"下载失败，请重新下载"
             break
-        pic_url = re.findall('"pic_url":"(.*?)",',result.text,re.S)
-        # print result.text
+        pic_url = re.findall('"image":"(.*?)",',result.text,re.S)
         print len(pic_url)
         if len(pic_url) == 0:
             print "#####done#####"
             break
         for each in pic_url:
+            # each.replace('\\', '')
+            each = each.encode('utf-8')
+            each = each.replace('\\', '')
             f.write(each+'\n')
             print '正在下载第'+str(i+1)+'张图片，图片地址:'+str(each)
             try:
@@ -54,7 +57,6 @@ def downloadPic(url,keyword, pages):
                     InvalidSchema, InvalidURL, URLRequired, ProxyError):
                 print '【错误】当前图片无法下载'
                 continue
-            string = 'pictures/'+keyword+'_'+str(i) + '.jpg'
             #resolve the problem of encode, make sure that chinese name could be store
             #fp = open(string.decode('utf-8').encode('cp936'),'wb')
             #fp.write(pic.content)
@@ -66,70 +68,30 @@ def downloadPic(url,keyword, pages):
             w = (image.shape)[1]
             max_length = w if w > h else h
             if max_length >= 300:
-                cv2.imwrite('pictures/sougou/'+keyword+'/'+keyword+'_'+str(i)+'.jpg', image)
+                cv2.imwrite('pictures/paixin/'+keyword+'_'+str(person_num)+sex+str(pages)+'/'+keyword+'_'+str(i)+'.jpg', image)
                 i+=1
     f.close()
 
 
 
 if __name__ == '__main__':
-    url = 'http://pic.sogou.com/pics?'
+    url = 'http://api.paixin.com/dp_media/search_list?'
+
+    keyword_list = ['自拍']
+    # downloadPic(url, keyword_list[0], 10000, 1, "male")
+    pool = multiprocessing.Pool(processes=40)
+    for word in keyword_list:
+        for index in range(10000, 110000, 10000):
+            pool.apply_async(downloadPic, (url, word, index, 2,))
+    for word in keyword_list:
+        for index in range(10000, 110000, 10000):
+            pool.apply_async(downloadPic, (url, word, index, 3,))
+    for word in keyword_list:
+        for index in range(10000, 110000, 10000):
+            pool.apply_async(downloadPic, (url, word, index, 1, "male",))##male
+            pool.apply_async(downloadPic, (url, word, index, 1, "female",))##female
 
 
-    # keyword_list = ['女主播', '女孩', '女生', '女老师', '女运动员', '女清洁工', '女教授', '女服务员', '女医生', '女演员', '女明星']
-    # # keyword_list = ['1', '2', '3', '4', '5', '6', '7']
-    # pool = multiprocessing.Pool(processes=50)
-    # for word in keyword_list:
-    #     ori_word = word
-    #     #downloadPic(url,ori_word, 30)
-    #     word_zipai = '自拍_'+ ori_word
-    #     pool.apply_async(downloadPic, (url, word_zipai, 1000,))
-    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_zipai, 1000,))
-    #     # p.start()
-    #     # p.join()
-    #     # downloadPic(url, ori_word, 1000)
-    #     word_changfa = '自拍_'+ori_word + '_长发'
-    #     pool.apply_async(downloadPic, (url, word_changfa, 1000,))
-    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_changfa, 1000,))
-    #     # p.start()
-    #     # p.join()
-    #     # downloadPic(url, word_changfa, 1000)
-    #     word_duanfa = '自拍_'+ori_word + '_短发'
-    #     pool.apply_async(downloadPic, (url, word_duanfa, 1000,))
-    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_duanfa, 1000,))
-    #     # p.start()
-    #     # p.join()
-    #     # downloadPic(url, word_duanfa, 1000)
-    #     word_zhongfa = '自拍_' + ori_word + '_中发'
-    #     pool.apply_async(downloadPic, (url, word_zhongfa, 1000,))
-    #     # p = multiprocessing.Process(target=downloadPic, args=(url, word_zhongfa, 1000,))
-    #     # p.start()
-    #     # p.join()
-    #     # downloadPic(url, word_duanfa, 1000)
-    # pool.close()
-    # pool.join()
-    # #
-    # #
-    keyword_list = ['情侣', '父子', '母子', '爸妈', '爷爷奶奶', '姐弟', '双胞胎', '龙凤胎']
-    pool = multiprocessing.Pool(processes=10)
-    for word in keyword_list:
-        ori_word = word
-        #downloadPic(url,ori_word, 30)
-        word_zipai = '自拍_'+ ori_word
-        pool.apply_async(downloadPic, (url, word_zipai, 1000,))
-        # downloadPic(url, word_zipai, 1000)
-        # word_changfa = ori_word + '长发'
-        # downloadPic(url, word_changfa, 30)
-        # word_duanfa = '自拍_'+ori_word + '短发'
-        # downloadPic(url, word_duanfa, 1000)
-        # word_duanfa = '自拍_' + ori_word + '中发'
-        # downloadPic(url, word_duanfa, 1000)
-    keyword_list = ['三口之家', '一家三口', '三胞胎', '三个人']
-    for word in keyword_list:
-        # keyword_list2 = ['_1', '_2', '_3', '_4']
-        # for word2 in keyword_list2:
-        # downloadPic(url, '自拍_'+word, 1000)
-        pool.apply_async(downloadPic, (url, '自拍_'+word, 1000,))
 
     pool.close()
     pool.join()
